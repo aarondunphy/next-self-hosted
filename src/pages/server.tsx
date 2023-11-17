3752d0032803d4abee5c943ad6a89bf194ff53eb
Aaron Dunphy <aaronjohndunphy@gmail.com>
2023-11-17 11:12:20 +0000

    Add links

--- src/pages/server.tsx
+++ src/pages/server.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from "next";
+import Link from "next/link";
 import React from "react";
 
 type ServerPageProps = {
@@ -6,7 +7,12 @@ type ServerPageProps = {
 };
 
 export default function ServerPage({ names }: ServerPageProps) {
-  return <div>Names: {names.join(", ")}</div>;
+  return (
+    <div>
+      <Link href="/">Go Home</Link>
+      <div>Names: {names.join(", ")}</div>
+    </div>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
